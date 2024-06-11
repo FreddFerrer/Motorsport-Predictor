8561a5bd32369945df6f4e569e46d70234b345e2
Freddy <ferreirafreddy39@gmail.com>
2024-06-11 18:44:32 -0300

    userController added

--- src/main/java/com/fredd/motorsport_predictor/controllers/PredictionGroupController.java
+++ src/main/java/com/fredd/motorsport_predictor/controllers/PredictionGroupController.java
@@ -4,6 +4,7 @@ package com.fredd.motorsport_predictor.controllers;
 import com.fredd.motorsport_predictor.dto.response.PredictionGroupDto;
 import com.fredd.motorsport_predictor.service.IPredictionGroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,16 +27,16 @@ public class PredictionGroupController {
         return ResponseEntity.of(iPredictionGroupService.getPredictionGroupById(id));
     }
 
-    //@GetMapping(path = "/usuario/{groupName}")
-    //public ResponseEntity<PredictionGroupDto> getPredictionGroupByUserCreator(@PathVariable String userCreator) {
-    //    return ResponseEntity.of(iPredictionGroupService.getPredictionGroupByUser(userCreator));
-    //}
+    @GetMapping(path = "/usuario/{groupName}")
+    public ResponseEntity<PredictionGroupDto> getPredictionGroupByUserCreator(@PathVariable Long userCreatorId) {
+        return ResponseEntity.of(iPredictionGroupService.getPredictionGroupByUser(userCreatorId));
+    }
 
-//    @PostMapping()
-//    public ResponseEntity<PredictionGroupResponseDto> save(@RequestBody PredictionGroupDto predictionGroupDto) {
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(iPredictionGroupService.savePredictionGroup(predictionGroupDto));
-//    }
+    //@PostMapping()
+    //public ResponseEntity<PredictionGroupResponseDto> save(@RequestBody PredictionGroupDto predictionGroupDto) {
+    //    return ResponseEntity.status(HttpStatus.CREATED)
+    //            .body(iPredictionGroupService.savePredictionGroup(predictionGroupDto));
+    //}
 
 
 
--- src/main/java/com/fredd/motorsport_predictor/controllers/UserController.java
+++ src/main/java/com/fredd/motorsport_predictor/controllers/UserController.java
@@ -1,12 +1,37 @@
 package com.fredd.motorsport_predictor.controllers;
 
+import com.fredd.motorsport_predictor.dto.response.UserDto;
+import com.fredd.motorsport_predictor.service.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final IUserService iUserService;
+
+    // Nuevo método para obtener todos los usuarios
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllUsers() {
+        List<UserDto> users = iUserService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    // Nuevo método para obtener un usuario por ID
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Long id) {
+        Optional<UserDto> user = iUserService.getUserById(id);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
 }
