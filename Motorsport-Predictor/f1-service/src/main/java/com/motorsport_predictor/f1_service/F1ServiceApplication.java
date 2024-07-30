dd82439d2a959f651aa5801b4d13534f8579be84
Freddy <ferreirafreddy39@gmail.com>
2024-07-30 01:34:11 -0300

    f1-service created

---
+++ Motorsport-Predictor/f1-service/src/main/java/com/motorsport_predictor/f1_service/F1ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.motorsport_predictor.f1_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1ServiceApplication.class, args);
+	}
+
+}
---
+++ Motorsport-Predictor/f1-service/src/test/java/com/motorsport_predictor/f1_service/F1ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorsport_predictor.f1_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
