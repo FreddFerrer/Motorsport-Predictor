4bc928dd9f854be9003b12d440178bb3a95834dd
Freddy <ferreirafreddy39@gmail.com>
2024-08-26 23:55:47 -0300

    scraper method added

---
+++ Motorsport-Predictor/predictions-service/src/main/java/com/motorsport_predictor/predictions_service/services/IScraperService.java
@@ -0,0 +1,5 @@
+package com.motorsport_predictor.predictions_service.services;
+
+public interface IScraperService {
+    void getResults();
+}
---
+++ Motorsport-Predictor/predictions-service/src/main/java/com/motorsport_predictor/predictions_service/services/impl/ScraperServiceImpl.java
@@ -0,0 +1,10 @@
+package com.motorsport_predictor.predictions_service.services.impl;
+
+import com.motorsport_predictor.predictions_service.services.IScraperService;
+
+public class ScraperServiceImpl implements IScraperService {
+    @Override
+    public void getResults() {
+
+    }
+}
