e1b73ef9e7875d22cda1a91cd53cfbb40ce4f4f2
Freddy <ferreirafreddy39@gmail.com>
2024-09-02 10:16:49 -0300

    openapi added

--- Motorsport-Predictor/f1-service/src/main/java/com/motorsport_predictor/f1_service/F1ServiceApplication.java
+++ Motorsport-Predictor/f1-service/src/main/java/com/motorsport_predictor/f1_service/F1ServiceApplication.java
@@ -1,11 +1,14 @@
 package com.motorsport_predictor.f1_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "F1 Service", version = "1.0", description = "Documentation F1 Service v1.0"))
 public class F1ServiceApplication {
 
 	public static void main(String[] args) {
--- Motorsport-Predictor/predictions-service/src/main/java/com/motorsport_predictor/predictions_service/PredictionsServiceApplication.java
+++ Motorsport-Predictor/predictions-service/src/main/java/com/motorsport_predictor/predictions_service/PredictionsServiceApplication.java
@@ -1,11 +1,14 @@
 package com.motorsport_predictor.predictions_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "Predictions Service", version = "1.0", description = "Documentation Predictions Service v1.0"))
 public class PredictionsServiceApplication {
 
 	public static void main(String[] args) {
